1baaf3d5ae9f2787a8c81b8804aedee7e7a1adfb
cartobaldrica <baldrica18@gmail.com>
2018-10-15 15:32:33 -0500

    Add files via upload

--- js/intro.js
+++ js/intro.js
@@ -49,9 +49,9 @@
       /* Previous button label in tooltip box */
       prevLabel: '&larr; Back',
       /* Skip button label in tooltip box */
-      skipLabel: 'Skip',
+      skipLabel: 'Exit',
       /* Done button label in tooltip box */
-      doneLabel: 'Done',
+      doneLabel: 'Exit',
       /* Hide previous button in the first step? Otherwise, it will be disabled button. */
       hidePrev: false,
       /* Hide next button in the last step? Otherwise, it will be disabled button. */
